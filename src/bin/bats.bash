4a79cc3bb78d28f38831af60db01c12d5188eb79
j5pu <j5pu@Mac-Pro-de-j5pu.local>
2022-02-21 13:46:02 +0100

    submodules

--- src/bin/bats.bash
+++ src/bin/bats.bash
@@ -1,3 +1,28 @@
-#!$SHEBANG$
+#!/bin/bash
 
-$END$
+: "${BASH_SOURCE:?Must Run in BASH}"
+
+basename="${BASH_SOURCE[0]##*/}"
+
+# GIT Super Project Top Level
+#
+export BATS_TOP
+
+if BATS_TOP="$(git rev-parse --show-superproject-working-tree --show-toplevel)"; then
+  cd "$(echo "${BATS_TOP}" | head -1)" || exit
+  directory="_${basename}"
+  if ! git config -f .gitmodules submodule."${basename}".path &>/dev/null; then
+    git submodule add --quiet --name "${basename}" "https://github.com/j5pu/${basename}.git" "${directory}"
+    git add .gitmodules
+    git commit --quiet -m "submodule: ${basename}"
+    git push --quiet
+  fi
+fi
+git submodule update --quiet --init --recursive "${directory}"
+
+if [ "${BASH_SOURCE[0]##*/}" = "${0##*/}" ]; then
+  "./${directory}/${basename}" "$@"
+else
+  . "./${directory}/${basename}" "$@"
+  unset basename directory
+fi
